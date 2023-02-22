e0186441dc9fec7c41427f1a7dc0c6b99aa652d6
Justin Glommen <jglommen@clickup.com>
2023-02-22 14:29:08 -0800

    chore: Upgrade projenrc file from JS -> TS

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,12 +1,13 @@
-const { clickupCdk } = require('@time-loop/clickup-projen');
+import { clickupCdk } from '@time-loop/clickup-projen';
 const project = new clickupCdk.ClickUpCdkConstructLibrary({
-  author: 'user',
-  authorAddress: 'user@domain.com',
+  author: 'ClickUp',
+  authorAddress: 'devops@clickup.com',
   cdkVersion: '2.1.0',
   defaultReleaseBranch: 'main',
   devDeps: ['@time-loop/clickup-projen'],
   name: 'cdk-s3-file-generator',
-  repositoryUrl: 'https://github.com/user/cdk-s3-file-generator.git',
+  repositoryUrl: 'https://github.com/time-loop/cdk-s3-file-generator.git',
+  projenrcTs: true,
 
   // deps: [],                /* Runtime dependencies of this module. */
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
